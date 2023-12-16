c562cbc7ba72e95ee9e30f339032914c0c2717ab
Darlan Bomfim <darlanbs05@gmail.com>
2023-12-16 11:09:52 -0300

    Feat:Criação dos Barbeiros

--- src/barbers/barbers.controller.ts
+++ src/barbers/barbers.controller.ts
@@ -1,7 +1,28 @@
-import { Controller } from '@nestjs/common';
-import { BarbersService } from './barbers.service';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  HttpStatus,
+  Post,
+  Res,
+} from "@nestjs/common";
+import { BarbersService } from "./barbers.service";
+import CreateBarberDto from "./dtos/create-barber";
+import { Response } from "express";
 
-@Controller('barbers')
+@Controller("barbers")
 export class BarbersController {
   constructor(private readonly barbersService: BarbersService) {}
+  @Post()
+  async create(@Body() data: CreateBarberDto, @Res() res: Response) {
+    const barberExists = await this.barbersService.findBarberByEmail(
+      data.email
+    );
+
+    if (barberExists) {
+      res.status(HttpStatus.CONFLICT).json({ message: "Barbeiro ja existe" });
+    }
+    const barber = await this.barbersService.createBarber(data);
+    return res.status(201).json(barber);
+  }
 }
--- src/barbers/barbers.service.ts
+++ src/barbers/barbers.service.ts
@@ -1,4 +1,14 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
+import { PrismaService } from "src/database/prisma.service";
+import CreateBarberDto from "./dtos/create-barber";
 
 @Injectable()
-export class BarbersService {}
+export class BarbersService {
+  constructor(private readonly prisma: PrismaService) {}
+  async findBarberByEmail(email: string): Promise<any> {
+     await this.prisma.barber.findFirst({ where: { email } });
+  }
+  async createBarber(data: CreateBarberDto) {
+    await this.prisma.barber.create({ data });
+  }
+}
