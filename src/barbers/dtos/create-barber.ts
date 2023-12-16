e03111063cbfa201d6c5e5e6c873f65f0143dade
Darlan Bomfim <darlanbs05@gmail.com>
2023-12-16 12:09:11 -0300

    Fix:Correção de numero de telefone ser opcional com o validator

--- src/barbers/dtos/create-barber.ts
+++ src/barbers/dtos/create-barber.ts
@@ -7,8 +7,6 @@ export default class CreateBarberDto {
   @IsNotEmpty({ message: "Email é obrigatorio" })
   @IsEmail({}, { message: "Email invalido" })
   email: string;
-  @Validate(PhoneNumberValidator, {
-    message: "O número de telefone não é válido.",
-  })
-  phone: string;
+  @Validate(PhoneNumberValidator)
+  phone?: string;
 }
--- src/barbers/dtos/phoneNumber_Validator.ts
+++ src/barbers/dtos/phoneNumber_Validator.ts
@@ -4,10 +4,13 @@ import {
   ValidationArguments,
 } from "class-validator";
 
-@ValidatorConstraint({ name: "phoneNumber", async: false })
+@ValidatorConstraint({ name: "phone", async: false })
 export class PhoneNumberValidator implements ValidatorConstraintInterface {
   validate(value: any, args: ValidationArguments) {
-    const phoneNumberRegex = /^\(?\d{2}\)?[-.\s]?\d{4,5}[-.\s]?\d{4}$/;
+    if (value === null || value === undefined || value === "") {
+      return true;
+    }
+    const phoneNumberRegex = /^$|^\(?\d{2}\)?[-.\s]?\d{4,5}[-.\s]?\d{4}$/;
     return phoneNumberRegex.test(value);
   }
 
