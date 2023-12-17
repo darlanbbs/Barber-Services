43f4525d91fe5d346306ea371147e6124845874a
Darlan Bomfim <darlanbs05@gmail.com>
2023-12-17 10:09:22 -0300

    Feat:Criação de filas e validação para ver se já existe uma

--- src/queue/queue.controller.ts
+++ src/queue/queue.controller.ts
@@ -1,7 +1,38 @@
-import { Controller } from '@nestjs/common';
-import { QueueService } from './queue.service';
+import { Body, Controller, Post, Res } from "@nestjs/common";
+import { QueueService } from "./queue.service";
+import { BarbersService } from "src/barbers/barbers.service";
+import createQueueDto from "./dtos/create-queue";
+import { Response } from "express";
 
-@Controller('queue')
+@Controller("queue")
 export class QueueController {
-  constructor(private readonly queueService: QueueService) {}
+  constructor(
+    private readonly queueService: QueueService,
+    private readonly barberServices: BarbersService
+  ) {}
+
+  @Post()
+  async createQueue(@Body() data: createQueueDto, @Res() res: Response) {
+    try {
+      const barberExist = await this.barberServices.findBarberById(
+        data.barberId
+      );
+      if (!barberExist) {
+        return res.status(404).json({ message: "Barbeiro inexistente" });
+      }
+      const existQueue = await this.queueService.verifyExistQueueToday(
+        data.barberId
+      );
+
+      if (existQueue) {
+        return res
+          .status(409)
+          .json({ message: "Ja existe uma chamada para o dia" });
+      }
+      const queue = await this.queueService.createQueue(data);
+      return res.status(201).json(queue);
+    } catch (error) {
+      return res.status(500).json({ message: "Erro interno do servidor" });
+    }
+  }
 }
--- src/queue/queue.service.ts
+++ src/queue/queue.service.ts
@@ -1,4 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
+import { PrismaService } from "src/database/prisma.service";
+import createQueueDto from "./dtos/create-queue";
 
 @Injectable()
-export class QueueService {}
+export class QueueService {
+  constructor(private readonly prisma: PrismaService) {}
+  async createQueue(data: createQueueDto) {
+    return await this.prisma.queue.create({ data });
+  }
+  async verifyExistQueueToday(barberId: string) {
+    return await this.prisma.queue.findFirst({
+      where: {
+        date: {
+          equals: new Date(),
+        },
+        barberId,
+      },
+    });
+  }
+}
