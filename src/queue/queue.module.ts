a8fe7b4214df749106d5853eed6e4441bb8368da
Darlan Bomfim <darlanbs05@gmail.com>
2023-12-17 10:09:04 -0300

    Fix?:Provendo servicos do barbeiro para criação de fila

--- src/queue/queue.module.ts
+++ src/queue/queue.module.ts
@@ -1,9 +1,10 @@
-import { Module } from '@nestjs/common';
-import { QueueService } from './queue.service';
-import { QueueController } from './queue.controller';
+import { Module } from "@nestjs/common";
+import { QueueService } from "./queue.service";
+import { QueueController } from "./queue.controller";
+import { BarbersService } from "src/barbers/barbers.service";
 
 @Module({
   controllers: [QueueController],
-  providers: [QueueService],
+  providers: [QueueService, BarbersService],
 })
 export class QueueModule {}
